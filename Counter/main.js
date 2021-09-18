170725ec2d202580fb7ea7a2d29dbf4740801b28
Miron <miron.ribkin98@mail.ru>
2021-09-18 19:31:39 +0300

    fixed

--- Counter/main.js
+++ Counter/main.js
@@ -1,12 +1,12 @@
 const plus = document.querySelector(".plus")
 const minus = document.querySelector(".minus")
-let counter = document.querySelector(".counter")
+const counter = document.querySelector(".counter")
 let number = 0;
-    plus.addEventListener('click', function (){
+plus.addEventListener('click', function () {
     number++
-        counter.innerHTML = number.toString();
-    })
-minus.addEventListener('click', function (){
+    counter.innerHTML = number.toString();
+})
+minus.addEventListener('click', function () {
     number--
     counter.innerHTML = number.toString();
-} )
+})
